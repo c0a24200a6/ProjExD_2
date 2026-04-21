3f8c3495dc7056463a4a860c7c02976d76b81726
hama <c0a24200a6@edu.teu.ac.jp>
2026-04-21 14:50:09 +0900

    練習3

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -9,6 +9,19 @@ os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 DELTA = {pg.K_UP: (0, -5), pg.K_DOWN: (0, 5), pg.K_LEFT: (-5, 0), pg.K_RIGHT: (5, 0)}
 
+def check_bound(rct, scr_rct):
+    """
+    [1] rct: こうかとん or 爆弾のRect
+    [2] scr_rct: スクリーンのRect
+    範囲内：+1／範囲外：-1
+    """
+    yoko, tate = +1, +1
+    if rct.left < scr_rct.left or scr_rct.right < rct.right:
+        yoko = -1
+    if rct.top < scr_rct.top or scr_rct.bottom < rct.bottom:
+        tate = -1
+    return yoko, tate
+
 
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
@@ -24,6 +37,7 @@ def main():
     bb_rct = bb_img.get_rect()
     bb_rct.center = random.randint(0, WIDTH), random.randint(0, HEIGHT)
     bb_img.set_colorkey((0, 0, 0))
+    bb_vx, bb_vy = +5, +5
 
     tmr = 0
     while True:
@@ -49,9 +63,18 @@ def main():
                 sum_mv[0] += move[0]
                 sum_mv[1] += move[1]
         kk_rct.move_ip(sum_mv)
+        if check_bound(kk_rct, screen.get_rect()) != (1, 1):
+            kk_rct.move_ip(-sum_mv[0], -sum_mv[1])
         screen.blit(kk_img, kk_rct)
-        bb_rct.move_ip(5, 5)  
+
+        bb_rct.move_ip(bb_vx, bb_vy)
+        yoko, tate = check_bound(bb_rct, screen.get_rect())
+        if yoko == -1:
+            bb_vx *= -1
+        if tate == -1:
+            bb_vy *= -1
         screen.blit(bb_img, bb_rct)
+
         pg.display.update()
         tmr += 1
         clock.tick(50)
